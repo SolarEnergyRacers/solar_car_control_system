ce7c71c2b0cec3ac2424dc83703611ce09f49ebd
Andreas Kuster <mail@andreaskuster.ch>
2021-02-03 01:16:00 +0100

    Add initial port of the ADS1x15 analog to digital converter library #7. Add comments.

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,40 @@
 /*
  *  TODO: add description & license
  */
+
+// standard library includes
 #include <stdio.h>
+
+// FreeRTOS / Arduino
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <driver/gpio.h>
-#include "sdkconfig.h"
 #include <Arduino.h>
 
-#include "driver/esp_system.h"
+// esp32 includes
+#include <driver/gpio.h>
+#include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "sdkconfig.h"
 
-#include "pinmap.h"
+// local includes
+#include "definitions.h"
+
+// external libs
+#include <Wire.h>
 
 
+// local libs
+#include <Adafruit_ADS1015.h>
+Adafruit_ADS1015 ads; // forAdafruit_ADS1115 ads;
+
+
+
+// add C linkage definition
 extern "C" {
     void app_main(void);
 }
 
-void blink_task(void *pvParameter)
-{
+void blink_task(void *pvParameter) {
     /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
        muxed to GPIO on reset already, but some default to other
        functions and need to be switched to GPIO. Consult the
@@ -29,7 +44,7 @@ void blink_task(void *pvParameter)
     gpio_pad_select_gpio(LED_BUILTIN);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
-    while(1) {
+    while (1) {
         /* Blink off (output low) */
         gpio_set_level(LED_BUILTIN, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -39,7 +54,7 @@ void blink_task(void *pvParameter)
     }
 }
 
-void chip_info(void *pvParameter){
+void chip_info(void *pvParameter) {
 
     // print chip information
     esp_chip_info_t chip_info;
@@ -55,8 +70,26 @@ void chip_info(void *pvParameter){
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 }
 
-void app_main()
-{
+void restart(void *pvParameter) {
+
+    // count time down
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    // actual restart
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+}
+
+void init_i2c(void){
+    Wire.begin(I2C_SDA, I2C_SCL, I2C_FREQ);
+}
+
+void app_main(void) {
+
     // init arduino library
     initArduino();
 

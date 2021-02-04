f74c813b90eabec76f9a197890031912bf9ac704
Andreas Kuster <mail@andreaskuster.ch>
2021-02-05 00:12:01 +0100

    Add global definitions and pinout info.

--- include/definitions.h
+++ include/definitions.h
@@ -5,6 +5,42 @@
 #define DEFINITIONS_H
 
 
+/*
+ * ESP32 JTAG Debug Probe Wiring
+ *  
+ *  ESP32  - Probe
+ *  --------------
+ *  GPIO12 - TDI
+ *  GPIO15 - TDO
+ *  GPIO13 - TCK
+ *  GPIO14 - TMS 
+ * 
+ *  Tutorial: https://medium.com/@manuel.bl/low-cost-esp32-in-circuit-debugging-dbbee39e508b
+ *
+ *  General ESP32 Pinout: https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
+ */
+
+
+/*
+ * ESP32 SPI
+ *
+ *  ESP32  - Probe
+ *  --------------
+ *  VSPI
+ *  GPIO23   MOSI
+ *  GPIO19   MISO
+ *  GPIO18   CLK
+ *  GPIO5    CS (first spi device)
+ *
+ *  HSPI
+ *  GPIO13   MOSI
+ *  GPIO12   MISO
+ *  GPIO14   CLK
+ *  GPIO15   CS
+ *
+ */
+#define SPI_CS_SDCARD 5
+
 #define LED_BUILTIN (gpio_num_t)22
 
 #define I2C_SDA 33

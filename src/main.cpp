b937714d18443dbc667d9c41e9a34038d8ea5445
Andreas Kuster <mail@andreaskuster.ch>
2021-02-05 22:36:41 +0100

    Refactor gpio blink and hardware interrupt handler #10.

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,11 @@
 #include <freertos/semphr.h>
 #include <Arduino.h>
 
-// esp32 includes
-#include <driver/gpio.h>
-
+// project variables
 #include "sdkconfig.h"
 
 // local definitions
 #include "definitions.h"
-
-// external libs
-#include <OneWire.h> // 1-Wire
-#include <Wire.h> // I2C
-#include <SPI.h> // SPI
 
 // local libs
 #include <system.h>
@@ -36,60 +29,9 @@
 #include <Temp.h>
 #include <Display.h>
 #include <SDCard.h>
+#include <gpio.h>
 
-// add C linkage definition
-extern "C" {
-    void app_main(void);
-}
-
-void blink_task(void *pvParameter) {
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
-    gpio_pad_select_gpio(LED_BUILTIN);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
-    while (1) {
-        /* Blink off (output low) */
-        gpio_set_level(LED_BUILTIN, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-        gpio_set_level(LED_BUILTIN, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
-
-portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
-volatile int interrupt_counter = 0;
-void IRAM_ATTR handle_gpio_interrupt();
-void register_gpio_interrupt(){
-
-    printf("[HW Interrupt] Register gpio interrupt pin %d (falling edge)\n", GPIO_INTERRUPT_PIN);
-    pinMode(GPIO_INTERRUPT_PIN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(GPIO_INTERRUPT_PIN), handle_gpio_interrupt, FALLING);
-
-}
-
-void IRAM_ATTR handle_gpio_interrupt() {
-    portENTER_CRITICAL_ISR(&mux);
-    interrupt_counter++;
-    portEXIT_CRITICAL_ISR(&mux);
-}
-
-void int_report(void *pvParameter){
-
-    while(1){
-
-        printf("[INT] Number of interrupts: %d\n", interrupt_counter);
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
-
-
+// (de-)activate functionality & devices
 #define BLINK_ON true
 #define ADC_ON true
 #define DS_ON true
@@ -99,7 +41,12 @@ void int_report(void *pvParameter){
 #define INT_ON true
 #define SD_ON true
 #define DISPLAY_ON true
+#define INT_ON true
 
+// add C linkage definition
+extern "C" {
+void app_main(void);
+}
 
 void app_main(void) {
 
@@ -115,40 +62,39 @@ void app_main(void) {
     init_spi();
 
     // init modules & create tasks
-    if(BLINK_ON){
-        xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+    if (BLINK_ON) {
+        xTaskCreate(&blink_demo_task, "blink_task", CONFIG_ESP_SYSTEM_EVENT_TASK_STACK_SIZE, NULL, 5, NULL);
     }
-    if(ADC_ON){
+    if (ADC_ON) {
         init_adc();
-        xTaskCreate(&read_adc_demo_task, "read_adc_task", 2500, NULL, 5, NULL);
+        xTaskCreate(&read_adc_demo_task, "read_adc_task", CONFIG_ESP_SYSTEM_EVENT_TASK_STACK_SIZE, NULL, 5, NULL);
     }
-    if(DS_ON) {
+    if (DS_ON) {
         init_ds();
-        xTaskCreate(&read_ds_demo_task, "read_ds_task", 2500, NULL, 5, NULL);
+        xTaskCreate(&read_ds_demo_task, "read_ds_task", CONFIG_ESP_SYSTEM_EVENT_TASK_STACK_SIZE, NULL, 5, NULL);
     }
-    if(GYRO_ACC_ON){
+    if (GYRO_ACC_ON) {
         init_gyro_acc();
-        xTaskCreate(&read_gyro_acc_demo_task, "read_gyro_acc_task", 2500, NULL, 5, NULL);
+        xTaskCreate(&read_gyro_acc_demo_task, "read_gyro_acc_task", CONFIG_ESP_SYSTEM_EVENT_TASK_STACK_SIZE, NULL, 5, NULL);
     }
-    if(PWM_ON) {
+    if (PWM_ON) {
         init_pwm();
-        xTaskCreate(&update_pwm_demo_task, "update_pwm_task", 2500, NULL, 5, NULL);
+        xTaskCreate(&update_pwm_demo_task, "update_pwm_task", CONFIG_ESP_SYSTEM_EVENT_TASK_STACK_SIZE, NULL, 5, NULL);
     }
-    if(RTC_ON) {
+    if (RTC_ON) {
         init_rtc();
-        xTaskCreate(&read_rtc_demo_task, "read_adc_task", 2500, NULL, 5, NULL);
+        xTaskCreate(&read_rtc_demo_task, "read_adc_task", CONFIG_ESP_SYSTEM_EVENT_TASK_STACK_SIZE, NULL, 5, NULL);
     }
-    if(SD_ON){
+    if (SD_ON) {
         init_sdcard();
-        xTaskCreate(&write_sdcard_demo_task, "write_sdcard_task", 2500, NULL, 5, NULL);
+        xTaskCreate(&write_sdcard_demo_task, "write_sdcard_task", CONFIG_ESP_SYSTEM_EVENT_TASK_STACK_SIZE, NULL, 5, NULL);
     }
-    if(INT_ON) {
-        // register interrupt handler
+    if (INT_ON) {
         register_gpio_interrupt();
-        xTaskCreate(&int_report, "int_report_task", 2500, NULL, 5, NULL);
+        xTaskCreate(&int_report_demo_task, "int_report_task", CONFIG_ESP_SYSTEM_EVENT_TASK_STACK_SIZE, NULL, 5, NULL);
     }
-    if(DISPLAY_ON){
+    if (DISPLAY_ON) {
         init_display();
-        xTaskCreate(&draw_display_demo_task, "display_task", 2500, NULL, 5, NULL);
+        xTaskCreate(&draw_display_demo_task, "display_task", CONFIG_ESP_SYSTEM_EVENT_TASK_STACK_SIZE, NULL, 5, NULL);
     }
 }

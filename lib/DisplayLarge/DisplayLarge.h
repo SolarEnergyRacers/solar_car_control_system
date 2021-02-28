f80e9d8a9b4fa0f9b352505fe9591a3a21a93e64
Karlheinz Schubert <ksc@gemeinsamlernen.ch>
2021-02-28 15:15:21 +0100

    feat: add linux port settings, initial init

---
+++ lib/DisplayLarge/DisplayLarge.h
@@ -0,0 +1,11 @@
+//
+// Display
+//
+
+#ifndef DISPLAY_LARGE_H
+#define DISPLAY_LARGE_H
+
+void init_display_large(void);
+void draw_display_large_demo_task(void *pvParameter);
+
+#endif // DISPLAY_H

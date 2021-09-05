19428d6c459379d3d01c53591784cccf990b2bfe
Karlheinz Schubert <ksc@gemeinsamlernen.ch>
2021-09-05 02:56:21 +0200

    technology backup

--- src/CarState.cpp
+++ src/CarState.cpp
@@ -2,20 +2,32 @@
 // Car State with all car information
 //
 
-#include <string>
-#include <definitions.h>
 #include <CarState.h>
 #include <DriverDisplayC.h>
+#include <definitions.h>
+#include <string>
 
 using namespace std;
 
-extern CONSTANT_MODE constant_drive_mode;
-extern bool constant_drive_set;
+// CarState::CarState() {
+//   Speed.set(0);
+//   Acceleration.set(0);
+//   Deceleration.set(0);
+//   Battery.set(-1);
+//   PhotoVoltaic.set(-1);
+//   Motor.set(-1);
+// }
 
 string CarState::printableState() {
-  stringstream ss;
-  ss << "Car state\nSpeed=" << Speed << "\nAcceleration=" << Acceleration;
-  return ss.str();
+  // stringstream ss;
+  // ss << "Car state\nSpeed=" << Speed << "\nAcceleration=" << Acceleration;
+  // return ss.str();
+  return "------";
 }
 
-string CarState::serializedState() { return "...."; }
+string CarState::serializedState() {
+  float a = 3;
+  float b = 5;
+  float c = abs(b - a);
+  return "....";
+}

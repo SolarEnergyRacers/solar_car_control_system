3eecac37414816dfac6afb16cb935bac696dc34a
Karlheinz Schubert <ksc@gemeinsamlernen.ch>
2021-10-03 09:44:28 +0200

    fix pins

--- lib/ADC/ADC.cpp
+++ lib/ADC/ADC.cpp
@@ -21,7 +21,7 @@ void ADC::re_init() { ADC::init(); }
 void ADC::init() {
 
   // instantiate the devices with their corresponding address
-  ads_addrs[0] = 0; //I2C_ADDRESS_ADS1x15_0;
+  ads_addrs[0] = I2C_ADDRESS_ADS1x15_0;
   ads_addrs[1] = 0; // I2C_ADDRESS_ADS1x15_1;
   ads_addrs[2] = I2C_ADDRESS_ADS1x15_2;
 

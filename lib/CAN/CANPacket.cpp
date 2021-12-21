5489891bc32b1bf2f6c7e24433eb730876c5085f
Andreas Kuster <mail@andreaskuster.ch>
2021-12-21 17:25:46 +0100

    Fix clang-format

--- lib/CAN/CANPacket.cpp
+++ lib/CAN/CANPacket.cpp
@@ -2,86 +2,63 @@
 
 #include "CANPacket.h"
 
-CANPacket::CANPacket(){
-    this->id = 0;
-    this->data = 0;
+CANPacket::CANPacket() {
+  this->id = 0;
+  this->data = 0;
 }
 
-CANPacket::CANPacket(uint16_t id, uint8_t data[]){
-    this->id = id;
-    
-    for(uint8_t i = 0; i < sizeof(data); i++){
-        this->data = this->data + (data[i] << (i * 8));
-    }
+CANPacket::CANPacket(uint16_t id, uint8_t data[]) {
+  this->id = id;
+
+  for (uint8_t i = 0; i < sizeof(data); i++) {
+    this->data = this->data + (data[i] << (i * 8));
+  }
 }
 
-CANPacket::CANPacket(uint16_t id, uint64_t data){
-    this->id = id;
-    this->data = data;
+CANPacket::CANPacket(uint16_t id, uint64_t data) {
+  this->id = id;
+  this->data = data;
 }
 
+void CANPacket::setID(uint16_t id) { this->id = id; }
 
-void CANPacket::setID(uint16_t id){
-    this->id = id;
+uint16_t CANPacket::getID() { return this->id; }
+
+void CANPacket::setData(uint8_t data[]) {
+  for (uint8_t i = 0; i < sizeof(data); i++) {
+    this->data = this->data + (data[i] << (i * 8));
+  }
 }
 
-uint16_t CANPacket::getID(){
-    return this->id;
+void CANPacket::setData(uint64_t data) { this->data = data; }
+
+uint64_t CANPacket::getData_ui64() { return this->data; }
+
+// ToDo Test unsigned->signed on ESP32 runtime
+int64_t CANPacket::getData_i64() {
+  int64_t result = this->data & 0xFFFFFFFFFFFFFFFF;
+  return result;
 }
 
-void CANPacket::setData(uint8_t data[]){
-    for(uint8_t i = 0; i < sizeof(data); i++){
-        this->data = this->data + (data[i] << (i * 8));
-    }
-}
+uint32_t CANPacket::getData_ui32(uint8_t index) { return (uint32_t)(this->data >> (32 * index)) & 0x00000000FFFFFFFF; }
 
-void CANPacket::setData(uint64_t data){
-    this->data = data;
-}
+int32_t CANPacket::getData_i32(uint8_t index) { return (int32_t)(this->data >> (32 * index)) & 0x00000000FFFFFFFF; }
 
-uint64_t CANPacket::getData_ui64(){
-    return this->data;
-}
+uint16_t CANPacket::getData_ui16(uint8_t index) { return (uint16_t)(this->data >> (16 * index)) & 0x000000000000FFFF; }
 
-//ToDo Test unsigned->signed on ESP32 runtime
-int64_t CANPacket::getData_i64(){
-    int64_t result = this->data & 0xFFFFFFFFFFFFFFFF;
-    return result;
-}
+int16_t CANPacket::getData_i16(uint8_t index) { return (int16_t)(this->data >> (16 * index)) & 0x000000000000FFFF; }
 
-uint32_t CANPacket::getData_ui32(uint8_t index){
-    return (uint32_t) (this->data >> (32*index)) & 0x00000000FFFFFFFF;
-}
+uint8_t CANPacket::getData_ui8(uint8_t index) { return (uint8_t)(this->data >> (8 * index)) & 0x00000000000000FF; }
 
-int32_t CANPacket::getData_i32(uint8_t index){
-    return (int32_t) (this->data >> (32*index)) & 0x00000000FFFFFFFF;
-}
+int8_t CANPacket::getData_i8(uint8_t index) { return (int8_t)(this->data >> (8 * index)) & 0x00000000000000FF; }
 
-uint16_t CANPacket::getData_ui16(uint8_t index){
-    return (uint16_t) (this->data >> (16*index)) & 0x000000000000FFFF;
-}
+bool CANPacket::getData_b(uint8_t index) { return ((this->data >> index) & 0x0000000000000001); }
 
-int16_t CANPacket::getData_i16(uint8_t index){
-    return (int16_t) (this->data >> (16*index)) & 0x000000000000FFFF;
-}
+float CANPacket::getData_f32(uint8_t index) {
+  uint32_t ival = (uint32_t)(this->data >> (32 * index)) & 0x00000000FFFFFFFF;
+  float fval = 0.0;
 
-uint8_t CANPacket::getData_ui8(uint8_t index){
-    return (uint8_t) (this->data >> (8*index)) & 0x00000000000000FF;
-}
+  memcpy(&fval, &ival, sizeof(fval));
 
-int8_t CANPacket::getData_i8(uint8_t index){
-    return (int8_t) (this->data >> (8*index)) & 0x00000000000000FF;
-}
-
-bool CANPacket::getData_b(uint8_t index){
-    return ((this->data >> index) & 0x0000000000000001);
-}
-
-float CANPacket::getData_f32(uint8_t index){
-    uint32_t ival = (uint32_t) (this->data >> (32*index)) & 0x00000000FFFFFFFF;
-    float fval = 0.0;
-    
-    memcpy(&fval, &ival, sizeof(fval));
-
-    return fval;
+  return fval;
 }

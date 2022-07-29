91434b3966b27dace3cecace9efd6e2eabeb8b01
Karlheinz Schubert <ksc@gemeinsamlernen.ch>
2022-07-29 19:58:44 +0200

    fix: improve output of cmd handling on console

--- lib/CmdHandler/CmdHandler.cpp
+++ lib/CmdHandler/CmdHandler.cpp
@@ -173,48 +173,51 @@ void CmdHandler::task() {
           memory_info();
           break;
         case 'i':
+          console << "Received: '" << input << "' --> ";
           if (input[1] == 'i') {
             ioExt.verboseModeDigitalIn = !ioExt.verboseModeDigitalIn;
-            console << "Received: " << input << " -->  set verboseModeDigitalIn: " << ioExt.verboseModeDigitalIn << "\n";
+            console << "set verboseModeDigitalIn: " << ioExt.verboseModeDigitalIn << "\n";
           } else if (input[1] == 'o') {
             ioExt.verboseModeDigitalOut = !ioExt.verboseModeDigitalOut;
-            console << "Received: " << input << " -->  set verboseModeDigitalOut: " << ioExt.verboseModeDigitalOut << "\n";
+            console << "set verboseModeDigitalOut: " << ioExt.verboseModeDigitalOut << "\n";
           } else if (input[1] == 'a') {
             adc.verboseModeADC = !adc.verboseModeADC;
-            console << "Received: " << input << " -->  set verboseModeADC: " << adc.verboseModeADC << "\n";
+            console << "set verboseModeADC: " << adc.verboseModeADC << "\n";
           } else if (input[1] == 'd') {
             dac.verboseModeDAC = !dac.verboseModeDAC;
-            console << "Received: " << input << " -->  set verboseModeDAC: " << dac.verboseModeDAC << "\n";
+            console << "set verboseModeDAC: " << dac.verboseModeDAC << "\n";
           } else if (input[1] == 'R') {
-            console << "Received: " << input << " -->  ioExt.re_init()\n";
+            console <<   ioExt.re_init()<< "\n";
             msg = ioExt.re_init();
             console << msg << "\n";
           } else {
             ioExt.readAllPins();
-            console << "Received: " << input << carState.printIOs("", true, false) << "\n";
+            console << carState.printIOs("", true, false) << "\n";
           }
           break;
         case 'I':
-          console << "Received: " << input << " -->  i2cBus.scan_i2c_devices()\n";
+          console << "Received: '" << input << "' -->  i2cBus.scan_i2c_devices()\n";
           i2cBus.scan_i2c_devices();
           break;
         // -------------- chase car commands
         case 'k':
 #if CARSPEED_ON
+          console << "Received: '" << input << "' --> ";
           if (input[1] == 'v') {
             carSpeed.verboseModePID = !carSpeed.verboseModePID;
           } else {
             string arr[4];
             int count = splitString(arr, &input[1]);
             if (count == 0) {
-              console << "PID settings Kp=" << carState.Kp << ", Ki=" << carState.Ki << ", Kd=" << carState.Kd << "\n";
+              console << "PID parameters: ";
             } else {
               float Kp = atof(arr[0].c_str());
               float Ki = atof(arr[1].c_str());
               float Kd = atof(arr[2].c_str());
               carSpeed.update_pid(Kp, Ki, Kd);
-              console << "PID set to   Kp=" << carState.Kp << ", Ki=" << carState.Ki << ", Kd=" << carState.Kd << "\n";
+              console << "PID set parameters: ";
             }
+            console << "Kp=" << carState.Kp << ", Ki=" << carState.Ki << ", Kd=" << carState.Kd << "\n";
           }
 #else
           console << "Car speed control deactivated\n";
@@ -223,7 +226,7 @@ void CmdHandler::task() {
         case 't': {
           time_t theTime = time(NULL);
           struct tm t = *localtime(&theTime);
-          console << "DateTime: " << asctime(&t) << "\n";
+          console << "Received: '" << input << "' --> DateTime: " << asctime(&t) << "\n";
         } break;
         case 'T': {
 #if RTC_ON
@@ -242,13 +245,13 @@ void CmdHandler::task() {
           esp32time.setTime(ss, MM, hh, dd, mm, yy);
           time_t theTime = time(NULL);
           struct tm t = *localtime(&theTime);
-          console << "Set dateTime to: " << asctime(&t) << "\n";
+          console << "Received: '" << input << "' --> Set dateTime to: " << asctime(&t) << "\n";
 #else
           console << "RTC deactivated\n";
 #endif
         } break;
         case '-':
-          console << "Received: " << input << " -->  carControl.adjust_paddles(" << carState.PaddleAdjustCounter << ")\n";
+          console << "Received: '" << input << "' -->  carControl.adjust_paddles(" << carState.PaddleAdjustCounter << ")\n";
           carControl.adjust_paddles(carState.PaddleAdjustCounter); // manually adjust paddles (3s handling time)
           break;
         case 'F':
@@ -259,7 +262,7 @@ void CmdHandler::task() {
             carState.Fan = true;
             carState.getPin(PinFanOut)->value = 1;
           }
-          console << "Received: " << input << " -->  carState.Fan=" << carState.Fan << "\n";
+          console << "Received: '" << input << "' -->  carState.Fan=" << carState.Fan << "\n";
           break;
         case 'G':
           if (input[1] == '-') {
@@ -269,7 +272,7 @@ void CmdHandler::task() {
             carState.GreenLight = true;
             carState.getPin(PinGreenLightOut)->value = 1;
           }
-          console << "Received: " << input << " -->  carState.GreenLight=" << carState.GreenLight << "\n";
+          console << "Received: '" << input << "' -->  carState.GreenLight=" << carState.GreenLight << "\n";
           break;
         case 'u':
           if (input[1] == '-') {
@@ -277,7 +280,7 @@ void CmdHandler::task() {
           } else {
             carState.SpeedArrow = SPEED_ARROW::INCREASE;
           }
-          console << "Received: " << input << " -->  carState.SpeedArrow=" << SPEED_ARROW_str[(int)(carState.SpeedArrow)] << "\n";
+          console << "Received: '" << input << "' -->  carState.SpeedArrow=" << SPEED_ARROW_str[(int)(carState.SpeedArrow)] << "\n";
           break;
         case 'd':
           if (input[1] == '-') {
@@ -285,35 +288,35 @@ void CmdHandler::task() {
           } else {
             carState.SpeedArrow = SPEED_ARROW::DECREASE;
           }
-          console << "Received: " << input << " -->  carState.SpeedArrow=" << SPEED_ARROW_str[(int)(carState.SpeedArrow)] << "\n";
+          console << "Received: '" << input << "' -->  carState.SpeedArrow=" << SPEED_ARROW_str[(int)(carState.SpeedArrow)] << "\n";
           break;
         case ':':
           carState.DriverInfoType = INFO_TYPE::INFO;
           carState.DriverInfo = &input[1];
-          console << "Received: " << input << " -->  carState.DriverInfo " << INFO_TYPE_str[(int)carState.DriverInfoType] << ": "
+          console << "Received: '" << input << "' -->  carState.DriverInfo " << INFO_TYPE_str[(int)carState.DriverInfoType] << ": "
                   << carState.DriverInfo << "\n";
           break;
         case '!':
           carState.DriverInfoType = INFO_TYPE::WARN;
           carState.DriverInfo = &input[1];
-          console << "Received: " << input << " -->  carState.DriverInfo " << INFO_TYPE_str[(int)carState.DriverInfoType] << ": "
+          console << "Received: '" << input << "' -->  carState.DriverInfo " << INFO_TYPE_str[(int)carState.DriverInfoType] << ": "
                   << carState.DriverInfo << "\n";
           break;
         // -------------- steering wheel input element emulators
         case '<':
           indicator.setIndicator(INDICATOR::LEFT);
           indicatorHandler();
-          console << "Received: " << input << " -->  carState.Indicator=" << INDICATOR_str[(int)(carState.Indicator)] << "\n";
+          console << "Received: '" << input << "' -->  carState.Indicator=" << INDICATOR_str[(int)(carState.Indicator)] << "\n";
           break;
         case '>':
           indicator.setIndicator(INDICATOR::RIGHT);
           indicatorHandler();
-          console << "Received: " << input << " -->  carState.Indicator=" << INDICATOR_str[(int)(carState.Indicator)] << "\n";
+          console << "Received: '" << input << "' -->  carState.Indicator=" << INDICATOR_str[(int)(carState.Indicator)] << "\n";
           break;
         case 'w':
           indicator.setIndicator(INDICATOR::WARN);
           indicatorHandler();
-          console << "Received: " << input << " -->  carState.Indicator=" << INDICATOR_str[(int)(carState.Indicator)] << "\n";
+          console << "Received: '" << input << "' -->  carState.Indicator=" << INDICATOR_str[(int)(carState.Indicator)] << "\n";
           break;
         case 'l':
           if (input[1] == '-') {
@@ -322,7 +325,7 @@ void CmdHandler::task() {
             carState.Light = LIGHT::L1;
           }
           light_switch();
-          console << "Received: " << input << " -->  carState.Light=" << LIGHT_str[(int)(carState.Light)] << "\n";
+          console << "Received: '" << input << "' -->  carState.Light=" << LIGHT_str[(int)(carState.Light)] << "\n";
           break;
         case 'L':
           if (input[1] == '-') {
@@ -331,7 +334,7 @@ void CmdHandler::task() {
             carState.Light = LIGHT::L2;
           }
           light_switch();
-          console << "Received: " << input << " -->  carState.Light=" << LIGHT_str[(int)(carState.Light)] << "\n";
+          console << "Received: '" << input << "' -->  carState.Light=" << LIGHT_str[(int)(carState.Light)] << "\n";
           break;
         case 'c':
           if (input[2] == '-') {
@@ -345,11 +348,11 @@ void CmdHandler::task() {
           } else {
             carState.ConstantModeOn = true; // #SAFETY#: deceleration unlock const mode
           }
-          console << "Received: " << input << " -->  carState.ConstantMode - " << CONSTANT_MODE_str[(int)(carState.ConstantMode)]
+          console << "Received: '" << input << "' -->  carState.ConstantMode - " << CONSTANT_MODE_str[(int)(carState.ConstantMode)]
                   << " On: " << carState.ConstantModeOn << "\n";
           break;
         default:
-          console << "Received: " << input << "\n";
+          console << "Received: '" << input << "'\n";
           console << "ERROR:: Unknown command '" << input << "' \n" << helpText << "\n";
           break;
         case 'h':
